7e6121bebfb4e5ecd7346fe63d8d946447ca42d3
William Patton <wjpatton@wjpatton.com>
2023-02-05 13:11:58 -0600

    Update main.tf
    Add Post Condition for Validation

--- main.tf
+++ main.tf
@@ -29,6 +29,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "aws_ami" "jammy" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -39,6 +55,14 @@ resource "aws_instance" "web" {
     owner = "Solutions Engineer"
     ttl   = "1"
 
+
+lifecycle {
+    postcondition {
+      condition     = self.ami == data.aws_ami.jammy.id
+      error_message = "Must use the latest available version of Ubuntu,
+        ${data.aws_ami.jammy.id}."
+    }
+  }
   }
 }
 
